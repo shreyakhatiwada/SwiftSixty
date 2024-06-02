f6b2028503a401d13091c99de76e175985d7697d
shreyakhatiwada <khatiwadashreya1@gmail.com>
2024-06-02 23:33:32 +0545

    enum

--- basics/basics .playground/Contents.swift
+++ basics/basics .playground/Contents.swift
@@ -202,25 +202,94 @@ let test = Square(sideLength: 5.3, name: "my test square")
 test.area()
 test.simpleDescription()
 
-class EquilateralTriangle : NamedShape {
-    var sidelenth  : Double = 0.0
-    
-    init(sideLength: Double, name: String)
-    {
-        self.sidelenth = sideLength
-        super.init(numberOfSides: 3, name: "equilateral traingle ")
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(numberOfSides: 3, name: name)
+    }
+
+
+    var perimeter: Double {
+        get {
+             return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
     }
 }
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
 
-class TriangleAndSquare{
+class TriangleAndSquare {
     var triangle: EquilateralTriangle {
         willSet {
-            square.sideLength = newValue.sidelenth
+            square.sideLength = newValue.sideLength
         }
     }
     var square: Square {
         willSet {
-            triangle.sidelenth = newValue.sideLength
+            triangle.sideLength = newValue.sideLength
         }
     }
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.triangle.sideLength)
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+
+
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+func compareRank(){
+    let jack = Rank.jack
+    let jackRawValue = jack.rawValue
+    let queen = Rank.queen
+    
+    if jack.rawValue < queen.rawValue {
+            print("jack is lower rank than queen with \(jackRawValue)")
+        } else {
+            print("jack is higher rank than queen")
+        }
+}
+compareRank()
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
 }
